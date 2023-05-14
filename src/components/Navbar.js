8cc5ce14a43d72fc1e1c1cb8a34dab62eb821e1d
Vihaan Singla <vihaansingla555@gmail.com>
2023-05-14 13:21:31 +0530

    some basics changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { FcDocument } from "react-icons/fc";
 function Navbar() {
+
+  const [untitled,setuntitled] = useState("Untitled Document");
   return (
     <div className='navbar'>
       <div className='left-side'>
         <div class="rename_area">
         <div className='logo'><FcDocument/></div>
-        <div><input type="text" id="united_text" value="Untitled document"></input></div>
+        <div><input type="text" id="united_text" value={untitled} onChange={(e)=>setuntitled(e.target.value)}></input></div>
         </div>
       </div>
       <div className='right-side'>
